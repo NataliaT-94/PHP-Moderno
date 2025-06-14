b0f626a7a8045dd9d45df2cd434786cecf0877e5
NataliaT-94 <nattecheira@gmail.com>
2025-06-14 16:51:47 -0300

    PHPModerno_Sintaxis_variables

---
+++ Sintaxis/variables.php
@@ -0,0 +1,26 @@
+<?php
+    // Las variables pueden iniciar con un guion bajo o con letra y con simbolo de dolar "$", no puede iniciar con numero.
+        $_variable;
+        $variable;
+        $variable2;
+
+    //Son sensibles a mayusculas y minusculas
+        $a = 1;
+        $A = 2;
+
+    //echo :imprime en pantalla
+
+    $name = "Natalia";
+    $age = 31;
+    //echo $name;
+
+    //Se pueden guardar elemento de tipo Boolean
+        $variable = true;
+
+    //Se puede concatenar
+
+        $message2 = "Mas info";
+        echo "Mi nombre es $name y tengo $age años ".$message2;
+
+
+?>
