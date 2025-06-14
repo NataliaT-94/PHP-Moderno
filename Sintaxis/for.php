0a69e2b6940ebd4b2d2cbecc6710a487ce7e5e74
NataliaT-94 <nattecheira@gmail.com>
2025-06-14 19:34:31 -0300

    PHPModerno_FOR

---
+++ Sintaxis/for.php
@@ -0,0 +1,12 @@
+<?php
+//for: Es una estructuras de control, llamadas de de repetición o iterativas, que son estructuras de control que sirven para hacer repeticiones.
+//Esta se conforma por tres partes.
+    //1 es la inicialización de los valores.
+    //2 es la condición que se tiene que cumplir para que esto se esté repitiendo.
+    //3 es qué pasa cuando termino la repetición?
+
+    for($i = 0; $i < 10; $i + 1){
+        echo $i;
+    }
+
+?>
