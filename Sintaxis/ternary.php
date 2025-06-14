8979d3945cc2589c1fd711591c119f7c6bb66def
NataliaT-94 <nattecheira@gmail.com>
2025-06-14 19:20:23 -0300

    PHPModerno_Ternary

---
+++ Sintaxis/ternary.php
@@ -0,0 +1,15 @@
+<?php
+//Estructura ternaria
+
+//es una forma compacta de hacer la expresión if else. Es decir, que vamos a escribir menos código para una condición verdadera y una falsa.
+
+//1 es la condición.
+//2 es si esta condición es verdadera, entonces aquí ponemos verdadero.
+//3 es si es falsa, entonces aquí ponemos falsa y de esta manera es como funciona el ternario.
+
+$value = true ? "verdadero" : "falsa";
+
+$age = 31;
+
+$value2 = $age > 18 ? "Mayor de edad" : "Menor";
+?>
