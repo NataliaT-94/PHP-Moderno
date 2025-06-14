b6b5ccffaaa23e789ad6c3937a9ab1c1f271fbe8
NataliaT-94 <nattecheira@gmail.com>
2025-06-14 17:03:35 -0300

    PHPModerno_dataType

---
+++ Sintaxis/dataType.php
@@ -0,0 +1,14 @@
+<?php 
+//Tipos de datos
+    //Especifica un alcance que puedes guardar en una variable
+
+    $number = 1;
+    $deciimal = 2.20;
+    $cadena = "esto es una cadena";
+    $boolean = false;
+    $vacio = null;
+
+
+
+
+?>
