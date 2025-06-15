1eb5b6d09ecbdcdddd292abbf4e53a7d3344f1b8
NataliaT-94 <nattecheira@gmail.com>
2025-06-15 14:43:40 -0300

    PHPModerno_POO_Class y Object

---
+++ POO/class.php
@@ -0,0 +1,18 @@
+<?php
+//Class:Una clase te permite crear objetos y el objeto es una estructura de datos, una colección de información, llamada propiedades y comportamientos
+
+$sale = new Sale();//objeto
+
+//accedemos a las propiedades del objeto
+$sale->total = 10.5;
+$sale->date = date("Y-m-d");
+print_r($sale);//imprimimos lo que contiene el objeto
+
+class Sale{//clase
+    public $total;
+    public $date;
+}
+
+
+
+?>
