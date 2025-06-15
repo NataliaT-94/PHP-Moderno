2b68da3776105b6510bcfca102a04dc1989c1842
NataliaT-94 <nattecheira@gmail.com>
2025-06-15 15:40:40 -0300

    PHPModerno_metodo y propiedades static

--- POO/class.php
+++ POO/class.php
@@ -14,6 +14,10 @@ $sale->createInvoice();
 //Constructores: es un tipo   de metodo especial. Para que yo obligue en la creación del objeto utilizando la clase que tenga que existir algo. Si no existe ese algo, no puedo crear el objeto.
 
 $sale = new Sale(10.5, date("Y-m-d"));
+
+echo Sale::$count;//accedemos por fuera
+
+Sale::reset();//llamamos al metodp estatico
  
 $sale->createInvoice();
 
@@ -21,11 +25,17 @@ $sale->createInvoice();
 class Sale{//clase
     public $total;
     public $date;
+    public static $count;//propiedad estatica
 
     public function __construct($total, $date)//le podemos asignar nosotros parámetros que van a ser obligados a utilizarse al momento que se crea el objeto.
     {
         $this->total = $total;//le asignamos propiedades a los parametros
         $this->date = $date;
+        self::$count++;//accedemos a la propiedad estatica de forma interna
+    }
+
+    public static function reset(){//metodo statico
+        self::$count = 0;
     }
 
     public function __destruct()
@@ -42,6 +52,6 @@ class Sale{//clase
 
 //destructor: Estos se ejecutan al momento que tu objeto ha sido dejado de utilizarse (Cuando se acaba el script). También cuando reemplazas el objeto, tu variable por otro nuevo objeto y sobre todo si es un objeto entre una función y esta función termina el destructor se invoca en automático, tu no lo invocas. Solamente puede existir una vez, pero el destructor no puede recibir parámetros y tampoco retorna información.
 
-
+//Metodos y propiedades estaticos: Pertenece a la class y no al object.
 
 ?>
