c20af415a04fc89d65e47484ba95f4f0859a38b9
NataliaT-94 <nattecheira@gmail.com>
2025-07-02 17:24:00 -0300

    PHPModerno_Pipe()

---
+++ programacionFuncional/pipe.php
@@ -0,0 +1,32 @@
+<?php 
+//Pipe(): es un concepto en el cual se aplican programación en front end y back end. Es más que nada una ejecución de funciones encadenadas y la recibiendo el resultado de la de la función anterior. Recibe la función que sigue el resultado y lo ejecuta y así sucesivamente hasta acabar el encadenamiento de funciones.
+
+//la desestructuración de argumentos de función.(...$nombre del array)
+
+function showNames(...$names){
+    foreach($names as $name){
+        echo $name."<br>";
+    }
+}
+
+showNames("Ana", "Juan", "Pedro");
+
+function pipe(...$funcs){
+    return function ($value) use ($funcs){
+        foreach($funcs as $fn){
+            $value = $fn($value);//el value obtenido en la funcion, se guarda en un value nuevamente
+        }
+
+        return $value;
+    };
+}
+
+$toUpper =fn ($s) => strtoupper($s);//transforma en mayuscula
+$replaceSpace = fn($s) => str_replace(" ", "", $s);//elimina los espacios vacios
+$replaceNumbers = fn($s) => preg_replace('/\d+/u', '', $s);//elimina los numeros
+
+$myPipe = pipe($toUpper, $replaceSpace, $replaceNumbers);
+$result = $myPipe("abcd ef891 gh");
+echo $result;
+
+?>
