f198610c7ba79edf4e3bf970e27cc890880c0661
Natalia T-94 <nattecheira@gmail.com>
2025-08-14 11:12:44 -0300

    PHP_Moderno/Programacion del metodo get() y exists() en el repository

--- http/httpcrud/data/Repository.php
+++ http/httpcrud/data/Repository.php
@@ -5,13 +5,27 @@ namespace app\data;
 
 use app\interfaces\RepositoryInterface;
 
-class Repository{
+class Repository implements RepositoryInterface{
     private string $fileData;
     private array $db;
 
     public function __construct(){
         $this->fileData = __DIR__.'/data.json';
         $json = file_get_contents($this->fileData);//en la variable $json se lee el archivo que esta en fileData
+        $this->db = json_decode($json, true);//deserializarmos el json para obtenerlo en un array
+    }
+
+    public function get(): array{
+        return $this->db;
+    }
+
+    public function exists(int $id): bool{
+        foreach ($this->db as $item) {
+            if($item["id"] == $id){
+                return true;
+            }
+        }
+        return false;
     }
 }
 
