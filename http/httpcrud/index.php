7adaf306d271aaf6e965d9fa1c36b2b26feb5c7e
Natalia T-94 <nattecheira@gmail.com>
2025-08-14 12:23:08 -0300

    PHP_Moderno/Integración de todas las partes

--- http/httpcrud/index.php
+++ http/httpcrud/index.php
@@ -1,23 +1,39 @@
 <?php 
 require_once __DIR__ . '/autoload.php';
 
-use\exceptions\DataException;
-use\exceptions\ValidationException;
+use app\business\Add;
+use app\business\Get;
+use app\business\Update;
+use app\business\Delete;
+use app\data\Repository;
+use app\validators\Validator;
+use app\exceptions\ValidationException;
+use app\exceptions\DataException;
+
+$repository = new Repository();
+$validator = new Validator();
 
 try{
 
     switch ($_SERVER['REQUEST_METHOD']) {//analiza un elemento y ve los casos distintos.
         case 'POST':
-            # code...
+            $body = json_decode(file_get_contents('php://input'), true);
+            $add = new Add($repository, $validator);
+            $add->add($body);
             break;
         case 'PUT':
-            # code...
+            $body = json_decode(file_get_contents('php://input'), true);
+            $update = new Update($repository, $validator);
+            $update->update($body);
             break;
         case 'DELETE':
-            # code...
+            $id = $_GET['id'];
+            $delete = new Delete($repository);
+            $delete->delete($body);
             break;
         case 'GET':
-            # code...
+            $get = new Get($repository);
+            echo json_encode($get->get());
             break;
         
         default:
--- http/httpcrud/validators/autoload.php
+++ http/httpcrud/validators/autoload.php
@@ -1,8 +1,17 @@
 <?php 
 //El objetivo de Autoload es ser un núcleo donde esté cargando bibliotecas, clases, etc...
 
-require_once __DIR__ . '/exceptions/DataException.php';//le indicamos donde se encuentra la excepcion
+require_once __DIR__ . '/interfaces/ValidatorInterface.php';
+require_once __DIR__ . '/interfaces/RepositoryInterface.php';
+
+require_once __DIR__ . '/validators/Validator.php';
 require_once __DIR__ . '/exceptions/ValidationException.php';
+require_once __DIR__ . '/exceptions/DataException.php';
+require_once __DIR__ . '/data/DataException.php';
+require_once __DIR__ . '/business/Get.php';
+require_once __DIR__ . '/business/Add.php';
+require_once __DIR__ . '/business/Update.php';
+require_once __DIR__ . '/business/Delete.php';
 
 
 
