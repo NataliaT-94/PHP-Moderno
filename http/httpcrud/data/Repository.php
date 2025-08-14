b4053955bdd2608c63bc2ee6686518d781c1f06e
Natalia T-94 <nattecheira@gmail.com>
2025-08-14 11:24:59 -0300

    PHP_Moderno/Programación del método create() en repository

--- http/httpcrud/data/Repository.php
+++ http/httpcrud/data/Repository.php
@@ -19,6 +19,17 @@ class Repository implements RepositoryInterface{
         return $this->db;
     }
 
+    public function create($data){
+        if(count($this->db) == 0){//si el db esta vacio
+            $data['id'] = 1; //la data agregada se va a tener un id 1
+        } else {//si la db no esta vacia
+            $lastElement = $this->db[count($this->db) - 1];//obtenemos la posicion del ultimo elemento
+            $data['id'] = ((int)$lastElement["id"]) + 1;//luego a la nueva informacion le colocamos el id que le sigue despues del ultimo elemento guardado
+        }
+        $this->db[] = $data;//por ultimo lo agregamos al array
+        file_put_contents($this->fileData, jron_encode($this->db));//lo guardamos en el archivo db
+    }
+
     public function exists(int $id): bool{
         foreach ($this->db as $item) {
             if($item["id"] == $id){
