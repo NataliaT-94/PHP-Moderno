244a7e8d38ff9074bc88ffad7ddd6ea79a35971f
Natalia T-94 <nattecheira@gmail.com>
2025-08-18 12:13:30 -0300

    PHP_Moderno/ Creación de archivo Excel utilizando biblioteca

--- composer/src/index.php
+++ composer/src/index.php
@@ -2,10 +2,25 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Carbon\Carbon;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 // echo "Hola desde composer";
 
 $now = Carbon::now();
-echo "La fecha y hora actual es: " . $now->toDateTimeString();
+//echo "La fecha y hora actual es: " . $now->toDateTimeString();
+
+$spreadsheet = new Spreadsheet();
+
+$sheet = $spreadsheet->getActiveSheet();
+
+$sheet->setCellValue('A1', 'Nombre');
+$sheet->setCellValue('A2', 'Natalia');
+
+$writer = new Xlsx($spreadsheet);
+$fileName = "exceles\myExcel.xlsx";
+$writer->save($fileName);
+
+echo "Archivo generado";
 
 ?>
