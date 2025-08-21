33091bd7ab0611a00addcae419ed3da8a266c2b2
Natalia T-94 <nattecheira@gmail.com>
2025-08-21 12:52:27 -0300

    PHP_Moderno/Ejecución del proyecto

--- project/src/data/BaseData.php
+++ project/src/data/BaseData.php
@@ -9,7 +9,7 @@ abstract class BaseData{
 
     public function __construct()
     {
-        $dsn = "mysql:host=". DB_HOST ."dbname=". DB_NAME .";charset=utf8";//conexion a la base de datos
+        $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8";
         $this->pdo = new PDO($dsn, DB_USER, DB_PASS);//creamos el objeto PDO, recibe la conexion dns, el usuario y contraseña
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//nos avisa si existe un error o excepcion
     }
--- project/src/index.php
+++ project/src/index.php
@@ -1,5 +1,25 @@
 <?php 
+require __DIR__ . '/../vendor/autoload.php';
 
+use app\interfaces\ExcelInterface;
+use app\interfaces\DataInterface;
+use app\data\BeerData;
+use app\excel\CreatorExcel;
+use app\business\GeneratorExcel;
+
+$now = date('Y-m-d');
+
+$filePath = __DIR__ . '/files/beer-' . $now . '.xlsx';
+
+
+$repository = new BeerData();//creamos el objeto encargado de la infomacion
+$excel = new CreatorExcel();//creamos el objeto encargado de crear el excel
+
+$generator = new GeneratorExcel($repository,$excel);//creamos el objeto encargado de general el excel mediante la informacion obtenida
+
+$generator->generate($filePath);
+
+echo "Excel creado";
 
 
 ?>
