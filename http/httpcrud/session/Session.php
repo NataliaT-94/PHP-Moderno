00840abb38442e470cfcc4d4f894b97b091cb40f
Natalia T-94 <nattecheira@gmail.com>
2025-08-21 13:20:52 -0300

    PHP_Moderno/Obtención de información de $_SESSION

--- http/httpcrud/session/Session.php
+++ http/httpcrud/session/Session.php
@@ -11,6 +11,33 @@ class Session implements RepositoryInterface{
             $_SESSION["berrs"] = [];//vamos a poner un array vacio
         }
     }
+
+    public function get(): array
+    {
+        return $_SESSION["beers"];
+    }
+
+    public function create($data)
+    {
+
+    }
+
+    public function update($data)
+    {
+
+    }
+
+    public function delete($id)
+    {
+
+    }
+
+    public function exists(int $id): bool
+    {
+        
+    }
+
+
 }
 
 ?>
